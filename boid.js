0cd324ef56d1f426c627b2a234609a9b2e5406ee
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2022-11-11 15:45:14 +0100

    Adding cohesion

--- boid.js
+++ boid.js
@@ -1,4 +1,89 @@
-class boid
+class Boid
   {
-    
+    constructor(x, y)
+    {
+      this.pos = createVector(float(x), float(y));
+      this.vel = createVector(0, 0);
+      this.acc = createVector(0, 0);
+      this.maxspeed = 3;
+      this.maxforce = 0.05;
+      this.r = 5;
+    }
+
+    // Method to update location
+    update(boids)
+    {
+      // Update position based on cohesion, separation, and alignment
+      var sep = this.separation(boids);
+      var ali = this.alignment(boids);
+      var coh = this.cohesion(boids);
+
+      print("Cohesion: " + coh);
+
+      this.vel.add(sep).add(ali).add(coh);
+      this.pos.add(this.vel);
+    }
+
+    draw()
+    {
+      // Draw a triangle rotated in the direction of velocity
+      var theta = this.vel.heading() + radians(90);
+      fill(127);
+      stroke(200);
+
+      push();
+
+      translate(this.pos.x, this.pos.y);
+
+      rotate(theta);
+
+      beginShape(TRIANGLES);
+
+      vertex(0, -this.r * 2);
+      vertex(-this.r, this.r * 2);
+      vertex(this.r, this.r * 2);
+
+      endShape(CLOSE);
+
+      pop();
+    }
+
+    cohesion(boids)
+    {
+      let max_dist = 50;
+
+      let sum = createVector(0, 0);
+
+      let count = 0;
+
+      for (let other of boids)
+      {
+        print("pos: " + this.pos + " other.pos: " + other.pos);
+        let d = dist(this.pos.x, this.pos.y, other.pos.x, other.pos.y);
+
+        if (d > 0 && d < max_dist)
+        {
+          sum.add(other.pos);
+
+          count++;
+        }
+      }
+
+      if (count > 0)
+      {
+        sum.div(count);
+
+        return sum.sub(this.pos).div(100.0);
+      }
+    }
+
+    separation(boids)
+    {
+     
+    }
+
+    alignment(boids)
+    {
+
+    }
   }
--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+boids = [];
+boids_num = 10;
+
 function setup() {
   createCanvas(400, 400);
+
+  for (let i = 0; i < boids_num; i++) {
+    boids.push(new Boid(random(width), random(height)));
+  }
 }
 
 function draw() {
   background(220);
+
+  for(let boid of boids) {
+    boid.update(boids);
+    boid.draw();
+  }
+
+  // noLoop();
 }
